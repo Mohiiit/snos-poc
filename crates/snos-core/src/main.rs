1d45e9298bf142f9ffa1ed3f14688cb5b41c6208
mohiiit <mohitdhattarwal.personal@gmail.com>
2025-09-09 17:20:20 +0530

    fix: missing field kind bug

--- crates/snos-core/src/main.rs
+++ crates/snos-core/src/main.rs
@@ -7,6 +7,27 @@ use snos_core::{generate_pie, ChainConfig, OsHintsConfiguration, PieGenerationIn
 // block 1943728 is now passing, after we sorted the attributes in order, same as it's done in the pathfinder
 // block 1943729 was also failing with the same error, testing that now
 // 1943729 failed with something else now, looking into it!
+
+// 🏷️  COMPILED CLASS HASH INCONSISTENT (some fixed, but getting new error of MAX_HIGH const not found)
+//    Description: Computed compiled_class_hash is inconsistent
+//    Count: 1018 (53.4%)
+//    Blocks: 1943728, 1943729, 1943787, 1943792, 1943808, 1943878, 1943879, 1943885, 1943906, 1943917, ... and 1008 more
+//
+// 🏷️  MISSING FIELD KIND (seems fixed, one block done, testing on more now)(tested two blocks and it worked for now)
+//    Description: Missing field `kind` (SerdeError)
+//    Count: 326 (17.1%)
+//    Blocks: 1943731, 1943799, 1943845, 1943913, 1943938, 1944008, 1944076, 1944077, 1944146, 1944215, ... and 316 more
+//
+// 🏷️  TRIE VALUE MISMATCH
+//    Description: Trie value mismatch (DiffAssertValues with patricia.cairo)
+//    Count: 549 (28.8%)
+//    Blocks: 1943743, 1943747, 1943750, 1943752, 1943753, 1943756, 1943768, 1943773, 1943774, 1943775, ... and 539 more
+//
+// 🏷️  OTHER ERRORS
+//    Description: All other uncategorized errors
+//    Count: 10 (0.6%)
+//    Blocks: 1944976, 1945751, 1945757, 1945779, 1945788, 1945818, 1945833, 1945850, 1946082, 1946705, ... and 2 more
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     env_logger::init();
@@ -15,7 +36,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // Build the input configuration
     let input = PieGenerationInput {
         rpc_url: "https://pathfinder-mainnet.d.karnot.xyz".to_string(),
-        blocks: vec![1943787], // 1872869 -> l1_handler, 1873475-> declare, 1873675 -> deploy account (from sepolia)
+        blocks: vec![1943743], // 1872869 -> l1_handler, 1873475-> declare, 1873675 -> deploy account (from sepolia)
         chain_config: ChainConfig::default(), // Uses Sepolia defaults
         os_hints_config: OsHintsConfiguration::default(), // Uses sensible defaults
         output_path: None,
